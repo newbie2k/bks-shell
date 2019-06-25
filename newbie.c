97764daf6a54f4f3444f2e6423cfcd582f8f9cbe
Bhaskar Singh <38446973+newbie2k@users.noreply.github.com>
2019-06-25 06:12:58 +0530

    Shell is taking variable number of arguments and options. fork() and execvp() used for testing purpose.
    Things needed to resolve 1. It should kept prompting shell prompt so that user can continue without any rerunning the program. 2. Needed to check if different array is needed for options or only argument array is sufficient"

--- newbie.c
+++ newbie.c
@@ -7,66 +7,145 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define SIZE 16
 #define SPACE 0
 #define NEWLINE 1
 
-short STATUS = SPACE;
+short PRESSED_KEY = SPACE;
 
 /**
- * getString Function will take variable character inputs
+ * readinput()
+ *
+ * This will take variable character inputs
  * until it reach to Space Character or NewLine Character.
  */
 
-char *getString(void) {
+char *readinput(void) {
 	char *word;
-	int c, n = 1;
-	STATUS = SPACE;
+	int c;      // store character temporarly
+	int n = 1;  // Used to increase the size of word
+	int count = 0;
 
-	word = malloc(sizeof(char) * 1);  // Assinged by malloc because word is
+	PRESSED_KEY = SPACE;
+
+	word =
+	    malloc(sizeof(char) * SIZE);  // Assinged by malloc because word is
 					  // going to used by realloc function.
 
 	while ((c = getchar()) != ' ') {
 		if (c == '\n') {
-			STATUS = NEWLINE;  // Used to Know how getString()
-					   // function return by pressing ENTER
-					   // KEY or SPACE KEY
+			/* Used to Know how readinput() function return by
+			   pressing ENTER KEY or SPACE KEY */
+			PRESSED_KEY = NEWLINE;
 			break;
 		}
-		word = realloc(word, n + 1);
-		word[n - 1] = c;
-		word[n] = '\0';
-		n++;
+
+		if (count == SIZE * n)  // Increasing Size
+			word = realloc(word, SIZE * n);
+
+		word[count++] = c;
+		word[count] = '\0';
 	}
 	return word;
 }
 
+
+int runcommand(char *cmd, char **opt, char **arg) {
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if(pid < 0) {
+		printf("Error while executing command!!!");
+	}
+	if(pid == 0) {
+		printf("Inside Parent\n ");
+		pid = wait(&status);
+		printf("Process with ID = %d ... Exited Succesfully\n",getpid());
+	}
+	if(pid > 0 ) {
+		printf("Inside Child Process PID = %d\n",pid);
+		execvp(cmd,arg);
+	}
+
+	return 0;
+}
 /**
  * Commands is the first word and words are seprated by Space,
  * Options must have dash(-) as prefix and argument are those words
  * which came after Options i.e. words which have dash(-) as prefix.
+ *
+ * COMMAND -OPTIONS(optional) ARGUMENT#1(optional) ARGUMENT#2(optional)
  */
 
 int main(void) {
-	char *cmd = NULL, *opt = NULL, *arg = NULL, *tmp = NULL;
+	char **opt;
+	char **arg;
+	char *tmp = NULL;
+	char *cmd = NULL;
+	char *usrname = NULL;  // USER Env Variable Value
+	char *path = NULL;     // PATH Env Variable Value
 
-	do {
-		printf("\nbks >$ ");
-		cmd = getString();
-		if (STATUS !=
-		    NEWLINE) {  // If ENTER KEY is not press i.e. we press SPACE
-				// KEY and about to give Options or Argument.
-			tmp = getString();
-			if (tmp[0] == '-') {  // If have Options
-				opt = tmp;
-				arg = getString();  // Argument After Options
-			} else
-				arg = tmp;  // Argument without Option
+	opt = malloc(sizeof(char *) * SIZE);
+	arg = malloc(sizeof(char *) * SIZE);
+	usrname = getenv("USER");
+	path = getenv("PATH");
+
+
+	while (1) {
+		int n = 1;  // Used to increase the size of word
+		int opt_count = 0;
+		int arg_count = 0;
+
+		printf("newbie@%s >$ ",usrname);  // Shell Prompt
+		cmd = readinput();
+
+		if ((strcmp(cmd, "quit") == 0) || (strcmp(cmd, "exit") == 0)) {  // Type "quit" or "exit" (without quots) to exit from program.
+			printf("\nThank You for Using newbie shell\n");
+			break;
 		}
-		printf("Command = %s, Argument = %s and Option = %s ", cmd, opt,
-		       arg);
 
-	} while ((strcmp(cmd, "quit") != 0) &&
-		 (strcmp(cmd, "exit") != 0));  // Type "quit" or "exit" (without
-					       // quots) to exit from program.
+		while (PRESSED_KEY == SPACE) {  // SPACE KEY pressed and about to give Options or Argument.
+			tmp = readinput();
+
+			if (tmp[0] == '-') {  // For Option
+
+				/* count start from 0 and we need one extra
+				   memory to store NULL thats way -2 insted of
+				   -1 */
+				if (opt_count == (SIZE * n) - 2) {
+					n++;
+					/* Increasing Size */
+					opt = realloc(opt, SIZE * n);
+				}
+				opt[opt_count] = tmp;
+				opt[opt_count + 1] = NULL;
+				opt_count++;
+			} else {  // For Argument
+				if (arg_count == (SIZE * n) - 2) {
+					n++;
+					/* Increasing Size */
+					arg = realloc(arg, SIZE * n);
+				}
+				arg[arg_count] = tmp;
+				arg[arg_count + 1] = NULL;
+				arg_count++;
+			}
+		runcommand(cmd, opt, arg);
+		}
+		/*printf("Command = %s\n", cmd);
+		for (int i = 0; *(arg + i) != NULL; i++)
+			printf("Arguments = %s\n", arg[i]);
+
+		for (int i = 0; *(opt + i) != NULL; i++)
+			printf("Options = %s\n", opt[i]); */
+	}
+		free(cmd);
+		free(arg);
+		free(opt);
 }
-
